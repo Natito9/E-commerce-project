4eb5d4193b62e6c8d0cf01b8d889be5e10e1048c
Natito9 <179492180+Natito9@users.noreply.github.com>
2024-12-02 08:20:03 +0100

    First commit, installed webpack and main files and set up.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # E-commerce-project
 Repo for collaborating in a group project to build an e-commerce website.
+
+HTML
+Each colaborator show work inside your own HTML. the name of this should be your section. These are the sections:
+home / product listning      
+product description card     
+Filter                       
+Shopping Cart                
+Checkout, summary & form     
+
+
+CSS 
+Create a separate module for your CSS, name it the same as your html. 
+
+Naming classes and Ids
+All classes or Ids must start with the corresponding section name or an abbreviation of it.
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 8080,
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: 'template.html', 
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
