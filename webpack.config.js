988cf6e20ef38fac50fa9c613776291f4edeaad3
zahrajoneydi <zahra.joneydi@hyperisland.se>
2024-12-05 13:02:03 +0100

    web pack configs

--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,22 @@ module.exports = {
     static: {
       directory: path.join(__dirname, "dist"),
     },
-    port: 8080,
+    port: 9001,
     watchFiles: ["./src/template.html"],
+    historyApiFallback: true, // Redirects all requests to template.html
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-      filename: 'template.html', 
+      filename: 'template.html',
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/pages/home.html',
+        filename: 'pages/home.html',
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/pages/checkout.html',
+        filename: 'pages/checkout.html',
     }),
   ],
   module: {
