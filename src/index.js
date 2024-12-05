defaec76e87ebf7e70398841afade09aaa63ba23
Sigstus985 <siggeluren@gmail.com>
2024-12-05 14:00:32 +0100

    modified raanas code to use the amount value

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './styles/reset.css';
 import './styles/style.css';
 import './styles/prod-listing.css'
+
 import { loadProducts } from './js/prod-listing.js';
 
-loadProducts();
+import { addToCart } from './js/addToCart.js';
+import { updateCart } from './js/addToCart.js';
+import { removeFromCart } from './js/addToCart.js';
 
+loadProducts();
