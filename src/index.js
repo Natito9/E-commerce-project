765f7731e0b4aa2cf40d3e06712377f73892f7f4
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2024-12-09 10:48:26 +0100

    Set up Jest and add filter.test.js

--- src/index.js
+++ src/index.js
@@ -3,25 +3,5 @@ import './styles/style.css';
 import './styles/prod-listing.css';
 import {registerRoute} from './js/router';
 
+
 registerRoute();
-
-
-/* import { loadProducts } from './js/prod-listing.js';
-import { fetchProductsCategories,createCategoryDropdown,setupCategoryFilter,renderProductCard  } from './js/filters.js' */
-/* 
-fetchProductsCategories();
-createCategoryDropdown();
-setupCategoryFilter();
-renderProductCard();
-
-loadProducts();
-
-import { addToCart } from './js/addToCart.js';
-import { updateCart } from './js/addToCart.js';
-import { removeFromCart } from './js/addToCart.js';
-import { registerRoute } from './js/router';
-
-import { addToCart } from './js/addToCart.js';
-import { updateCart } from './js/addToCart.js';
-import { removeFromCart } from './js/addToCart.js'; */
-
