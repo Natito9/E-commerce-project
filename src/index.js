b6d31350b7d9126b15d462efb96c90673685ec0a
zahrajoneydi <zahra.joneydi@hyperisland.se>
2024-12-09 13:42:53 +0100

    fixed css/responsiveness

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles/reset.css';
 import './styles/style.css';
 import './styles/prod-listing.css'
+import './styles/footer.css'
+import './styles/header.css'
 
 import { registerRoute } from './js/router';
 
