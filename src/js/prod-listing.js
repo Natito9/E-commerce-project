a3ed4638ebe0bf8ec2373eb405c4bd3c758e845a
Mandylaaane <ljungamanda@gmail.com>
2024-12-09 11:45:10 +0100

    small CSS updates on Prod.listing, removed unused div&lines from .card in Prod.listing JS.

--- src/js/prod-listing.js
+++ src/js/prod-listing.js
@@ -22,24 +22,6 @@ export async function loadProducts() {
             />
             <p class="price">Price: $${element.price}</p>
             <button class="add-to-cart" data-id="${element.id}" >Add to Cart</button>
-            <div class="shine"></div>
-            <div class="background">
-                <div class="tiles">
-                    <div class="tile tile-1"></div>
-                    <div class="tile tile-2"></div>
-                    <div class="tile tile-3"></div>
-                    <div class="tile tile-4"></div>
-                    <div class="tile tile-5"></div>
-                    <div class="tile tile-6"></div>
-                    <div class="tile tile-7"></div>
-                    <div class="tile tile-8"></div>
-                    <div class="tile tile-9"></div>
-                    <div class="tile tile-10"></div>
-                </div>
-                <div class="line line-1"></div>
-                <div class="line line-2"></div>
-                <div class="line line-3"></div>
-            </div>
         </div>`;
     container.innerHTML += card;
   });
