f5521557920bfb52ffdb222fd0ec90910327f848
Sigstus985 <siggeluren@gmail.com>
2024-12-10 12:07:19 +0100

    styling

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,4 +1,4 @@
-import "../styles/checkout-style.css"
+import "../styles/checkout-style.css";
 import { cart } from "./addToCart";
 
 //This function displays the current CART including the item amount and prices
@@ -12,24 +12,36 @@ export async function renderCart() {
 
 	const checkoutItems = document.querySelector("#checkout-items");
 	//This loops through the shopping cart array and renders out their images, names and total prices (calculated by the price * the Amount)
-	
+
 	//add eventlisteners for shipping
 	document.querySelector("#checkout-shipping").addEventListener("click", () => {
-		calculateTotalPrice()
-	})
+		calculateTotalPrice();
+	});
 
 	//add eventlisteners for payment options
 	//maybe now?
 	document.querySelector("#checkout-payment").addEventListener("click", () => {
-		togglePayment(document.querySelector("#checkout-payment").value)
-	})
+		togglePayment(document.querySelector("#checkout-payment").value);
+	});
+
+	//add eventlistener for submitting
+	document
+		.querySelector("#checkout-form")
+		.addEventListener("submit", (event) => {
+			event.preventDefault();
+			document.querySelector("#checkout-page").innerHTML = "";
+			const thanks = document.createElement("h1")
+			thanks.classList.add("thank-you")
+			thanks.textContent = "Thank you for your purchase^^"
+			document.querySelector("#checkout-page").appendChild(thanks)
+		});
 
 	for (let i = 0; i < cart.length; i++) {
 		const item = document.createElement("div");
-		item.classList.add("product")
+		item.classList.add("product");
 		item.innerHTML = `
         <img src="${cart[i].image}" class="checkout-product-img">
-		<p>${cart[i].price}</p>
+		<p>${cart[i].price}$</p>
         <h3>${cart[i].title}</h3>
         <h3 class="amount">${cart[i].Amount}</h3>
 		<h3>${Math.round(cart[i].price * cart[i].Amount * 100) / 100}$</h3>
@@ -48,34 +60,32 @@ export async function calculateTotalPrice() {
 		checkoutSum += cart[i].price * cart[i].Amount;
 	}
 	let totalPrice = checkoutSum + Number(shippingPrice);
-	renderTotalPrice(totalPrice)
+	renderTotalPrice(totalPrice);
 }
 
 export async function renderTotalPrice(totalPrice) {
 	const shippingEl = document.querySelector("#checkout-shipping-cost");
-	const orderSummary = document.querySelector("#checkout-order-summary");
 	const totalEL = document.querySelector("#checkout-total-price");
 	shippingEl.textContent = `Shipping: ${
 		document.querySelector("#checkout-shipping").value
 	}$`;
 	if (cart.length === 0) {
-		console.log("You have nothing in your cart bozo")
-		 orderSummary.innerHTML = `<h1> You don't have any items in your cart:( </h1>`;
-		/* document.querySelector("#checkout-form-section").innerHTML = ""; */
+		document.querySelector("#checkout-form-section").innerHTML =
+			"<h1>You have nothing in your cart bozo </h1>";
 	}
 	totalEL.textContent = `Total: ${totalPrice.toFixed(2)}$`;
 }
 
 export async function togglePayment(decider) {
 	if (decider == 0) {
-		document.querySelector("#checkout-credit-card").style.display = "none"
-		document.querySelector("#checkout-paypal").style.display = "grid"
-		console.log("true")
+		document.querySelector("#checkout-credit-card").style.display = "none";
+		document.querySelector("#checkout-paypal").style.display = "grid";
+		console.log("true");
 	} else {
-		document.querySelector("#checkout-credit-card").style.display = "grid"
-		document.querySelector("#checkout-paypal").style.display = "none"
-		console.log("false")
+		document.querySelector("#checkout-credit-card").style.display = "grid";
+		document.querySelector("#checkout-paypal").style.display = "none";
+		console.log("false");
 	}
 }
 
-window.onload(renderCart)
+window.onload(renderCart);
